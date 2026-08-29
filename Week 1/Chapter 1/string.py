08124e268116611813bd81c1b11b67bf7fd38cdc
Sandhya C <sandhyachamaraju@gmail.com>
2026-08-29 12:43:31 +0530

    Delete Week 1/Chapter 1/string.py

--- Week 1/Chapter 1/string.py
+++
@@ -1,8 +0,0 @@
-def has_double_space(text):
-    return "  " in text
-
-test_string="Hello sandhya"
-if has_double_space(test_string):
-    print("Double space detected!")
-else:
-    print("No double space found")
