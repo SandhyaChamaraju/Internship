0f43d36e88f6763618db4861f3c76ce69e009e3a
Sandhya C <sandhyachamaraju@gmail.com>
2026-09-07 21:09:10 +0530

    Update c-10 calculator.py

--- Week 1/Chapter 10/c-10 calculator.py
+++ Week 1/Chapter 10/c-10 calculator.py
@@ -13,8 +13,8 @@ class Calculator:
     def square_root(self):
         return math.sqrt(self.number)
 
-# Testing the calculator
 calc = Calculator(9)
-print(f"Square: {calc.square()}")         # Outputs: 81
-print(f"Cube: {calc.cube()}")             # Outputs: 729
-print(f"Square Root: {calc.square_root()}") # Outputs: 3.0
+print(f"Square: {calc.square()}")        
+print(f"Cube: {calc.cube()}")            
+print(f"Square Root: {calc.square_root()}") 
+
