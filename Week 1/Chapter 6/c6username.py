784182c58622874a629f42ce755ead58701f425b
Sandhya C <sandhyachamaraju@gmail.com>
2026-09-07 21:26:34 +0530

    Update c6username.py

--- Week 1/Chapter 6/c6username.py
+++ Week 1/Chapter 6/c6username.py
@@ -1,7 +1,5 @@
-#Prompt the user to enter a username
 username = input("Enter your username: ")
 
-#Check if the length of the username is less than 10 characters
 if len(username) < 10:
     print("The username contains less than 10 characters.")
 else:
