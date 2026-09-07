fa7de512f8a95ae9a47a5069eecd2ad4ce23e119
Sandhya C <sandhyachamaraju@gmail.com>
2026-09-07 21:03:46 +0530

    Update os.py

--- Week 1/Chapter 1/os.py
+++ Week 1/Chapter 1/os.py
@@ -3,10 +3,7 @@
 #Date:16-08-2026
 
 import os
+path="."
 
-# Specify your target directory path
-path="." #"." represents the current working directory
-
-# Get and print all file and folder names
 contents = os.listdir(path)
 print(contents)
