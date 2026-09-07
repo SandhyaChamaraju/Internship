44f48e84debb6997726cd7ef0177542f920cf314
Sandhya C <sandhyachamaraju@gmail.com>
2026-09-07 21:16:51 +0530

    Update c-13 pip freeze.py

--- Week 1/Chapter 13/c-13 pip freeze.py
+++ Week 1/Chapter 13/c-13 pip freeze.py
@@ -1,19 +1,13 @@
 from flask import Flask, jsonify, request
 
-# 1. Initialize the Flask application
 app = Flask(__name__)
-
-# 2. Define the Home route (Root URL)
 @app.route("/", methods=["GET"])
 def home():
     return "<h1>Welcome to my Flask Web Server!</h1><p>Go to /api/info to see JSON data.</p>"
 
-# 3. Define a dynamic route that accepts a parameter in the URL
 @app.route("/user/<username>", methods=["GET"])
 def greet_user(username):
     return f"Hello, {username}! Welcome back."
-
-# 4. Define an API route returning structured JSON data
 @app.route("/api/info", methods=["GET"])
 def get_info():
     server_data = {
@@ -24,7 +18,6 @@ def get_info():
     }
     return jsonify(server_data)
 
-# 5. Run the web server
 if __name__ == "__main__":
     # debug=True automatically reloads the server when you change code
     app.run(host="127.0.0.1", port=5000, debug=True)
