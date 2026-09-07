9f6a7180ee2f6b04faf333f31cf5c5b0073b2b9c
Sandhya C <sandhyachamaraju@gmail.com>
2026-09-07 20:59:49 +0530

    Update CRUD operation.py

--- Week 2/CRUD operation.py
+++ Week 2/CRUD operation.py
@@ -1,22 +1,14 @@
-#CRUD Operations
-
-
-# Create
 items = ["Apple", "Banana", "Orange"]
 print("Initial list:", items)
 
-# Read
 print("First item:", items[0])
 print("All items:", items)
 
-# Update
 items[1] = "Mango"
 print("After update:", items)
 
-# Delete
 items.remove("Orange")
 print("After delete:", items)
 
-# Add (part of Create)
 items.append("Grapes")
 print("After adding:", items)
